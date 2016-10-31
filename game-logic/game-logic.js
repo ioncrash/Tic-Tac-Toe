9a34954e56c4917075e5fbda3d8170677784c4a5
ioncrash <ioncrash@gmail.com>
2016-10-31 09:23:26 -0400

    create game logic and board storage
    board storage based on 3D array because I'm crazy

--- game-logic/game-logic.js
+++ game-logic/game-logic.js
@@ -1,3 +1,7 @@
+'use strict';
+
+let game = require('../board.js');
+
 const sample = ["o","x","o",
                 "x","o","x",
                 "o","x","o"];
@@ -5,7 +9,7 @@ const sample = ["o","x","o",
 const solutions = [
                   [[1,2],[4,8],[3,6]],
                   [[4,7],[0,2]],
-                  [[1,0],[4,6],[5,8]],
+                  [[0,1],[4,6],[5,8]],
                   [[0,6],[4,5]],
                   [[0,8],[1,7],[2,6],[3,5]],
                   [[2,8],[3,4]],
@@ -14,8 +18,10 @@ const solutions = [
                   [[0,4],[2,5],[6,7]]
                   ];
 
-const hasWon = function(board, move, solutions) {
+const hasWon = function(board, move) {
+  // find solutions for this position
   let solutionSet = solutions[move];
+  // check each solution against the board
   for (let i = 0; i < solutionSet.length; i++) {
         if (board[solutionSet[i][0]] === board[solutionSet[i][1]] &&
           board[solutionSet[i][0]] === board[move]) {
@@ -24,3 +30,11 @@ const hasWon = function(board, move, solutions) {
   }
   return false;
 };
+
+const playerMove = function(move) {
+
+};
+
+module.exports (
+  hasWon
+);
